429dbdea9a8426aa81b7919e1fa2a65cda07cfa0
Gerardo Felmer <desarrollo@satelnet.cl>
2020-12-17 00:16:58 -0300

    se modifica maven para que corra a full

--- maven.groovy
+++ maven.groovy
@@ -7,24 +7,24 @@
 def call(){
   
             stage("Build") {
-                sh "./mvnw clean compile -e"
+                sh "mvn clean compile -e"
             }
             stage("Test") {
-                sh "./mvnw clean test -e"
+                sh "mvn clean test -e"
             
             }
             stage("Jar") {
-                sh "./mvnw clean package -e"
+                sh "mvn clean package -e"
                 
             }
 
             stage('SonarQube') {
-                withSonarQubeEnv(installationName: 'sonar') {
-                    sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                withSonarQubeEnv(installationName: 'sonar-server') {
+                    sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
                  }
              }
             stage("Upload Nexus") {
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/Users/nicolas/code/estudios/usach/unidad3/forks/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
             }  
 }
 
