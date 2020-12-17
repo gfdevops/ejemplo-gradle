3683dff900f55b3b8797e8e4c228a0cd51ed5657
Gerardo Felmer <desarrollo@satelnet.cl>
2020-12-16 23:39:17 -0300

    se cambia a test-repo en nexus

--- gradle.groovy
+++ gradle.groovy
@@ -25,7 +25,7 @@ def call(){
                         sh "curl -X GET 'http://localhost:8085/rest/mscovid/test?msg=testing'"
                     }
                     stage("Nexus") {
-                        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', 
+                        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', 
                         packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', 
                         filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], 
                         mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'DevOpsUsach2020', version: '1.0.0']]]
