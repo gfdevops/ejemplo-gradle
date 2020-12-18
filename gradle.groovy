176568f85020ea5b8c7ebf33a9bba79681ac4626
Gerardo Felmer <desarrollo@satelnet.cl>
2020-12-18 00:23:56 -0300

    se cambia la forma de setear variables globales again

--- gradle.groovy
+++ gradle.groovy
@@ -6,28 +6,28 @@
 
 def call() {
     stage('Build & test') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh './gradlew clean build'
         sh './gradlew clean build'
     }
     stage('Sonar') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         def scannerHome = tool 'sonar-scanner'
         withSonarQubeEnv('sonar-server') { // If you have configured more than one global server connection, you can specify its name
             sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
         }
     }
     stage('Run') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh 'bash gradlew bootRun &'
         sleep 20
     }
     stage('Rest') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh "curl -X GET 'http://localhost:8085/rest/mscovid/test?msg=testing'"
     }
     stage('Nexus') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo',
                         packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar',
                         filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']],
--- maven.groovy
+++ maven.groovy
@@ -6,26 +6,26 @@
 
 def call() {
     stage('Build') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh 'mvn clean compile -e'
     }
     stage('Test') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh 'mvn clean test -e'
     }
     stage('Jar') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         sh 'mvn clean package -e'
     }
 
     stage('SonarQube') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         withSonarQubeEnv(installationName: 'sonar-server') {
             sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
         }
     }
     stage('Upload Nexus') {
-        withEnv(["JENKINS_STAGE=${env.STAGE_NAME}"])
+        env.JENKINS_STAGE = "${env.STAGE_NAME}"
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
     }
 }
